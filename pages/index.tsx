85a131de52996e680dd38c6317bfc28148de5a90
Miltiadis Koutsokeras <miltos@langaware.com>
2023-05-25 14:33:45 +0300

    Implementation of ephemeral test session URL generation complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from "react"
+import Link from 'next/link';
+
 import styles from '../styles/Home.module.css'
+import packageInfo from "../package.json"
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [ephemeralURL, setEphemeralURL] = useState('')
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,37 +20,61 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <header className={styles.header}>
         <Image src="/langaware-logo.webp" alt="LANGaware Logo"
           width={230} height={101} />
 
-        <h1 className={styles.title}>
-          langaware_test_session_url_generator
-        </h1>
+        <div className={styles.title_container}>
+          <h2>
+            Web Application for generating Test session ephemeral URLs.
+          </h2>
+        </div>
+      </header>
 
-        <h2>MiltiadisKoutsokeras</h2>
+      <main className={styles.main}>
+        <div>
+          <button onClick={
+            async function generate_url() {
+              setLoading(true)
 
-        <h2>
-          TODO Project Description
-        </h2>
+              fetch("/api/generate_url").then((resp) => {
+                resp.json().then((resp) => {
+                  setLoading(false)
+                  setEphemeralURL(resp.url)
+                  setError('')
+                }).catch((resp) => {
+                  setError('Cannot read returned ephemeral URL!')
+                  setLoading(false)
+                })
+              }).catch((resp) => {
+                setError('Cannot get ephemeral URL!')
+                setLoading(false)
+              })
+            }
+          }>Generate Ephemeral URL</button>
+
+          {loading && <div>Please wait...</div>}
+          {error != '' && <div>{error}</div>}
+          {ephemeralURL != '' && ClickHereLink(ephemeralURL)}
+        </div>
       </main>
 
       <footer className={styles.footer}>
-        All Rights Reserved
+        <div>All Rights Reserved</div>
 
         <a href="https://www.langaware.com" target="_blank"
           rel="noopener noreferrer">Powered by LANGaware</a>
 
-        {/* TODO: Add links to Privacy Policy and Terms of Use */}
-        <a href="https://www.langaware.com/TODO" target="_blank"
-          rel="noopener noreferrer">Privacy Policy</a>
-
-        <a href="https://www.langaware.com/TODO" target="_blank"
-          rel="noopener noreferrer">Terms of Use</a>
-
-        {/* TODO: Print Application version */}
-        Version: TODO
+        <div>Version: {packageInfo.version}</div>
       </footer>
+    </div>
+  )
+}
+
+function ClickHereLink(url: string) {
+  return (
+    <div>
+      Click <Link href={url} target='_blank'>here</Link> to perform a LANGaware test session
     </div>
   )
 }
